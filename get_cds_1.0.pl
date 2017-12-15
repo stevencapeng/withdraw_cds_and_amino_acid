c575ce62c4123b1da63d0b1c87b322a64f920b7a
stevencapeng <34545756+stevencapeng@users.noreply.github.com>
2017-12-15 18:41:14 +0800

    Add files via upload
    To get the cds, i withdraw the cds sequence parts and reverse the seq at first. And then replace the AGCT with TCGA in the sequence and print them after combined them.

---
+++ get_cds_1.0.pl
@@ -0,0 +1,45 @@
+($data,$data2)=@ARGV;#genome.fa  genome.gff
+my $row=0;
+$col2=0;
+open(IN,$data)||die"can not open";
+while (<IN>) {
+ chomp;
+ @str[$row]=$_;
+ $row=$row+1;
+ }
+open(IN,$data2)||die"can not open";
+while (<IN>) {
+ chomp;
+ @arr=split(/\t/,$_);
+  if ($arr[2]=~/CDS/) {
+   $arr[8]=~/Parent=(.*);/;
+   $x=$1;
+   if ($arr[6]=~/^-$/){
+   $sequence=substr($str[1],$arr[3]-1,$arr[4]-$arr[3]+1);
+     $sequence=reverse($sequence);
+     $sequence=~s/A/X/g;
+     $sequence=~s/G/Y/g;
+     $sequence=~s/T/A/g;
+     $sequence=~s/C/G/g;
+     $sequence=~s/X/T/g;
+     $sequence=~s/Y/C/g;
+   }
+   else {
+   $sequence=substr($str[1],$arr[3]-1,$arr[4]-$arr[3]+1);
+   }
+   if (exists($seq{$x})) {
+   $seq{$x}=$seq{$x}.$sequence;
+   }
+   else {
+   $seq{$x}=$sequence;
+   $parent{$col2}=$x;
+   $col2+=1;
+   }
+   }
+  }
+for($col1=0;$col1<=$col2-1;$col1+=1) {
+ print ">$parent{$col1}\n$seq{$parent{$col1}}\n";
+ }
+
+   
+      
